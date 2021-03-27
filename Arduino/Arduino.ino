a4e2e1d16f8b66d8b30697842e18509fd27307e0
dleyvacastro <dleyvacastro@gmail.com>
2021-03-27 12:15:25 -0500

    Se añadio el "hola mundo" del acelerometro

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,22 +1,26 @@
-#include <SoftwareSerial.h>
+#include <SoftwareSerial.h>  // libreria que permite establecer pines digitales
+        // para comunicacion serie
 
-SoftwareSerial Lily(10,11);
+//SoftwareSerial miBT(0, 1);  // pin 10 como RX, pin 11 como TX
+SoftwareSerial miBT(10,11);
 
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.print(9600);
-  Lily.begin(38400);
+
+void setup(){
+  Serial.begin(9600);   // comunicacion de monitor serial a 9600 bps
+  Serial.println("Listo");  // escribe Listo en el monitor
+
+  pinMode(10, INPUT);
+  pinMode(11, OUTPUT);
+  
+  miBT.begin(38400);    // comunicacion serie entre Arduino y el modulo a 38400 bps
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  if(Lily.available()){
-    Serial.write(Lily.read());
-  }
+void loop(){
+if (miBT.available())       // si hay informacion disponible desde modulo
+   Serial.write(miBT.read());   // lee Bluetooth y envia a monitor serial de Arduino
 
+if (Serial.available())     // si hay informacion disponible desde el monitor serial
+   miBT.write(Serial.read());   // lee monitor serial y envia a Bluetooth
 
-  if(Serial.available()){
-    Lily.write(Serial.read());
-  }
 }
