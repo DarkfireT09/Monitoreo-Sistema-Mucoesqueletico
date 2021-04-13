90ae08a30b293952a4a6752b116d18bbed81c02e
dleyvacastro <dleyvacastro@gmail.com>
2021-04-12 23:02:50 -0500

    Prueba de creacion de documento acelerometro

--- test_datos.py
+++ test_datos.py
@@ -1,36 +1,18 @@
-import serial
+import serial, time
+from Funciones import *
+# arduino = serial.Serial('COM7', 57600)
 
-arduino = serial.Serial('COM6', 57600)
-
-
-def get_information(s):
-    info = s.split(",")
-    infofloat = []
-    for i in info:
-        infofloat.append(float(i))
-
-    return infofloat
-
-def acelerometro(): 
-    """
-    Se obtiene informacion de la forma aceleracion(a), rotacion(g)
-    (ax,ay,az,gx,gy,gz)
-
-    Se devuelve la lista l. 
-    """
-    # a = random.randint(0,100)
-    a = arduino.readline()
-    a = str(a)
-    a = a[2:len(a)-5]
-    l = get_information(a)
-    return l
-
+texto_acelerometro = open('acelerometro.txt', 'w')
 
 cont = 0
 
 while cont < 10:
-    print(acelerometro())
+    # print(acelerometro())
+    info_acelerometro = acelerometro()
+    t = time.strftime('[%Y-%m-%d, %H:%M:%S]',time.localtime())
+    texto_acelerometro.write(t + str(info_acelerometro) + '\n')
     cont += 1
 
 
 arduino.close()
+texto_acelerometro.close()
