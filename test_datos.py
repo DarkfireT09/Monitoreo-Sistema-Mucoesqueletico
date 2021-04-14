c7098cccd3377770fac44e7e02600bb2e86dffd1
dleyvacastro <dleyvacastro@gmail.com>
2021-04-14 14:16:09 -0500

    Datos de sensor

--- test_datos.py
+++ test_datos.py
@@ -1,4 +1,4 @@
-import serial, time
+import serial
 from Funciones import *
 # arduino = serial.Serial('COM7', 57600)
 texto_acelerometro = open("acelerometro.txt", 'w')
@@ -7,22 +7,35 @@ k = time.perf_counter()
 r = time.perf_counter()
 while time.perf_counter() <= r+60:
     if time.perf_counter() <= k+10:
-        arduino.write("a".encode())
-        a = arduino.readline()
-        a = a.decode()
-        # a = acelerometro()
-        t = time.strftime('%Y-%m-%d,%H:%M:%S',time.localtime())
-        texto_acelerometro.write(t + ',' + str(a) + '\n')
-        print(a)
+        try:
+            # arduino.write("a".encode())
+            # a = arduino.readline()
+            # a = a.decode()
+
+            # # a = acelerometro()
+            # t = time.strftime('%Y-%m-%d,%H:%M:%S',time.localtime())
+            # texto_acelerometro.write(t + ',' + str(a) + '\n')
+            # print(a)
+            get_data("a", texto_acelerometro, 6)
+        except:
+            arduino.close()
+            texto_acelerometro.close()
+            texto_pulsioximetro.close()
     else:
-        arduino.write("p".encode())
-        p = arduino.readline()
-        p = p.decode()
-        # p = pulsioximetro()
-        t = time.strftime('%Y-%m-%d,%H:%M:%S',time.localtime())
-        texto_pulsioximetro.write(t + ',' + str(p) + '\n')
-        print(p)
-        k = time.perf_counter()
+        # arduino.write("p".encode())
+        # p = arduino.readline()
+        # p = p.decode()
+        # # p = pulsioximetro()
+        # t = time.strftime('%Y-%m-%d,%H:%M:%S',time.localtime())
+        # texto_pulsioximetro.write(t + ',' + str(p) + '\n')
+        # print(p)
+        try:
+            get_data("p", texto_pulsioximetro, 2)
+            k = time.perf_counter()
+        except:
+            arduino.close()
+            texto_acelerometro.close()
+            texto_pulsioximetro.close()
 arduino.close()
 texto_acelerometro.close()
 texto_pulsioximetro.close()
