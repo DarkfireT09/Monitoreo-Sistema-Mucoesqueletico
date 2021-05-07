d65936dcfc83b6dd1c18439d9230c4eb080b74af
David <daalmega@gmail.com>
2021-05-06 21:37:34 -0500

    Doble conexion con la base de datos exitosa

--- PrototipoWebFlask/MonitoreoFlask.py
+++ PrototipoWebFlask/MonitoreoFlask.py
@@ -16,39 +16,61 @@ try:
 except:
     raise NameError("La conexion con la base de datos ha fallado.")
 
+# Conexion con la base de datos
+try:
+    notification_connection = psycopg2.connect(host='127.0.0.1', port='5432',
+                                  dbname='Cornerstone', user='postgres',
+                                  password='1234')
+
+    print("Conexion con la base de datos exitosa!")
+except:
+    raise NameError("La conexion con la base de datos ha fallado.")
+
 
 cursor = connection.cursor()
+notification_cursor = notification_connection.cursor()
 app = Flask(__name__)
+
+# try:
+#     cursor.execute(
+#         """
+#         INSERT INTO notificaciones (fecha, mensaje, correo_usuario)
+#         VALUES (now()::timestamp, 'Prueba 2', 'david.melendez@urosario.edu.co')
+#         """
+#     )
+#     connection.commit()
+# except Exception as e:
+#     connection.rollback()
+#     print("No username Found")
 
 try:
     cursor.execute(
         """
-        SELECT * FROM Usuario
-        WHERE Correo = 'david.melendez@urosario.edu.co'
+        SELECT nombre, apellido FROM usuario
+        WHERE correo = 'david.melendez@urosario.edu.co'
         """
     )
     rows = cursor.fetchall()
-except:
+    nombre = rows[0][0]
+    apellido = rows[0][1]
+except Exception as e:
+    connection.rollback()
     print("No username Found")
 
-nombre = rows[0][1]
-apellido = rows[0][2]
-print(rows)
 
 # Notificaciones
 
 # Asignar el numero de notifcaciones
 Modules.global_variables.numero_notificaciones_actuales = \
-    Modules.Notifications.get_number_of_notifications(cursor)
+    Modules.Notifications.get_number_of_notifications(notification_connection.cursor())
 
-Modules.Notifications.manage_notifications(cursor)
+Modules.Notifications.manage_notifications(notification_cursor)
 
 
 
 
 @app.route('/')
 def index():
-
     return render_template('index.html', NOMBRE=nombre, APELLIDO=apellido)
 
 
@@ -118,7 +140,7 @@ def update_notif():
     try:
         cursor.execute(
             """
-            SELECT pulso,fecha
+            SELECT pulso, fecha
                 FROM pulsometro
                 ORDER BY fecha desc
             """
@@ -127,12 +149,15 @@ def update_notif():
     except:
         print("No data found")
 
-    pulso = rows[0][0]
-    fecha = rows[0][1]
-    return jsonify({
-        'pulso': pulso,
-        'fecha': fecha.strftime("%A:%H:%M:%S")
-    })
+        pulso = rows[0][0]
+        fecha = rows[0][1]
+        return jsonify({
+            'pulso': pulso,
+            'fecha': fecha.strftime("%A:%H:%M:%S")
+        })
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+    pass
 
