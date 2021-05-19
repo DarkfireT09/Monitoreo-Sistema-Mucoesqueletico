fd36fc18b729b0a52090db639eaebb92af01756a
dleyvacastro <62482485+dleyvacastro@users.noreply.github.com>
2021-05-19 08:26:21 -0500

    Update propotipo1.py

--- propotipo1.py
+++ propotipo1.py
@@ -3,81 +3,24 @@ import pandas as pd #para crear tablas donde se almacenaran los datos monitoread
 
 #-------------------INICIO DEL PROGRAMA--------------
 def main():
-    k = time.perf_counter()
-
-    texto_acelerometro = open('acelerometro.txt', 'w')
-    texto_pulsioximetro = open('pulsioximetro.txt', 'w')
     while True:
-        """
-        Ciclo cada 25 min lee el pulsioximetro, si detecta
-        valores anomalos manda alerta
-        """
-        if time.perf_counter() <= k+1500:
-
-            """
-            El formato actual es:
-                Acelerometro: YYYY,MM,DD,AX,AY,AZ,GX,GY,GZ
-                Pulsioximetro: YYYY,MM,DD,P,O
-            """
-
-            try:
-                """
-                Obtencion de datos, pasado a lista.
-                """
-                l = get_data("a", texto_acelerometro, 6)
-                l = l.split(",")
-                print(l)
-                
-                """
-                SISTEMA DE ALERTA
-                """
-                if(float(l[6]) < -6):
-                    alerta(l, texto_acelerometro)
-                time.sleep(1)
-            except:
-                arduino.close()
-                texto_acelerometro.close()
-                texto_pulsioximetro.close()
-                raise ValueError("Error en acelerometro")
-
-        else:
-             # NO implementado, se obtiene por defecto 0,1.
-
-            try:
-                l = get_data("p", texto_acelerometro, 2)
-                l = l.split(",")
-
-                """
-                Alertas aun no implementadas para el pulsioximetro
-                Fallo del modulo
-                """
-                if(float(l[6]) < 66):
-                    alerta(l, texto_pulsioximetro)
-                time.sleep(1)
-            except:
-                arduino.close()
-                texto_pulsioximetro.close()
-                texto_acelerometro.close()
-                raise ValueError("Error en pulsioximetro")
-
-
-            k = time.perf_counter()
-    else:
-        print("")
-        time.sleep(1.5)
-        print ("El dispositivo se apagará en ", end="")
-        time.sleep(2.5)
-        print ("3 ", end="")
-        time.sleep(1)
-        print ("2 ", end="")
-        time.sleep(1)
-        print ("1 ...")
-        time.sleep(1)
-
-        print ("¡Hasta la próxima!")
-    arduino.close()
-    texto_pulsioximetro.close()
-    texto_acelerometro.close()
+        lectura = arduino.readline()
+        lectura = lectura.decode()
+        lectura = lectura.replace("\r", '')
+        lectura = lectura.replace("\n", '')
+        lectura = lectura.split(",")
+        if lectura != ['']:
+            if len(lectura) == 3:
+                print("Acelerometro: ", lectura)
+                data_base_send_acelerometer(conexion, lectura[0], lectura[1], lectura[2], 'ejemplo@ejemplo.com')
+            else:
+                if int(lectura[1]) == 1:
+                    print("Alerta")
+                    data_base_send_notificacion(conexion, "Mala posicion detectada", 'ejemplo@ejemplo.com')
+                else:
+                    print("Pulsometro: ", lectura)
+                    data_base_send_notificacion(conexion, "Pon tu dedo en el sensor.", 'ejemplo@ejemplo.com')
+                    data_send_pulsometer(conexion, lectura[1], 'ejemplo@ejemplo.com')
 
 
 main()
