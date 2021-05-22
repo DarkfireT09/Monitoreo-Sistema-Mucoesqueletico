926cfc0e18b830b09e31e63ea406ec0e5da7abf5
dleyvacastro <62482485+dleyvacastro@users.noreply.github.com>
2021-05-22 11:12:38 -0500

    Update propotipo1.py

--- propotipo1.py
+++ propotipo1.py
@@ -4,28 +4,28 @@ import pandas as pd #para crear tablas donde se almacenaran los datos monitoread
 #-------------------INICIO DEL PROGRAMA--------------
 def main():
     while True:
-        lectura = arduino.readline()
-        lectura = lectura.decode()
-        lectura = lectura.replace("\r", '')
-        lectura = lectura.replace("\n", '')
-        lectura = lectura.split(",")
-        if lectura != ['']:
-            if len(lectura) == 3:
+        try:
+            lectura = arduino.readline()
+            lectura = lectura.decode()
+            lectura = lectura.replace("\r", '')
+            lectura = lectura.replace("\n", '')
+            lectura = lectura.split(",")
+    
+            if int(lectura[3]) == 1:
+                print("Alerta: ", lectura)
+                data_base_send_notificacion(conexion, "Mala posicion detectada, angulo = {0}".format(lectura[4]), 'ejemplo@ejemplo.com')
+            elif lectura[0] == "p":
+                print("Pulsometro: ", lectura)
+                data_base_send_notificacion(conexion, "Pon tu dedo en el sensor.", 'ejemplo@ejemplo.com')
+                data_send_pulsometer(conexion, lectura[1], 'ejemplo@ejemplo.com')
+            else:
                 print("Acelerometro: ", lectura)
                 data_base_send_acelerometer(conexion, lectura[0], lectura[1], lectura[2], 'ejemplo@ejemplo.com')
-            else:
-                if int(lectura[1]) == 1:
-                    print("Alerta")
-                    data_base_send_notificacion(conexion, "Mala posicion detectada", 'ejemplo@ejemplo.com')
-                else:
-                    print("Pulsometro: ", lectura)
-                    data_base_send_notificacion(conexion, "Pon tu dedo en el sensor.", 'ejemplo@ejemplo.com')
-                    data_send_pulsometer(conexion, lectura[1], 'ejemplo@ejemplo.com')
+        except:
+            pass
 
 
 main()
 
 
 #Apagar
-
-
