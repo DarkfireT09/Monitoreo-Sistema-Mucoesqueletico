83538c083cdc87512ea66dcf5351cd157e6a14e0
dleyvacastro <62482485+dleyvacastro@users.noreply.github.com>
2021-05-22 11:30:15 -0500

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -20,6 +20,11 @@ MPU6050 sensor;
 int ax, ay, az;
 int gx, gy, gz;
 
+long tiempo_prev;
+float dt;
+float ang_x, ang_y;
+float ang_x_prev, ang_y_prev;
+
 // Pulsometro
 const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
 const int LED13 = 13;          // The on-board Arduino LED, close to PIN 13.
@@ -53,8 +58,13 @@ void setup() {
 
 void loop() {
     float avrBPM = 0;
+    
     for (cont = 0; cont < 1800; cont++) {
+
+        // Leer las aceleraciones y velocidades angulares
+        sensor.getAcceleration(&ax, &ay, &az);
         sensor.getRotation(&gx, &gy, &gz);
+        
         float gx_deg_s = gx * (250.0 / 32768.0);
         float gy_deg_s = gy * (250.0 / 32768.0);
         float gz_deg_s = gz * (250.0 / 32768.0);
@@ -64,15 +74,26 @@ void loop() {
         Serial.print(gy_deg_s);
         Serial.print(",");
         Serial.println(gz_deg_s);
-        
+
+        //Calcular los angulos de inclinacion:
+        float accel_ang_x=atan(ax/sqrt(pow(ay,2) + pow(az,2)))*(180.0/3.14);
+        float accel_ang_y=atan(ay/sqrt(pow(ax,2) + pow(az,2)))*(180.0/3.14);
+        //Mostrar los angulos separadas por un [tab]
+        Serial.print("Inclinacion en X: ");
+        Serial.print(accel_ang_x); 
+        Serial.print("tInclinacion en Y:");
+        Serial.println(accel_ang_y);
+
         BT.print(gx_deg_s);
         BT.print(",");
         BT.print(gy_deg_s);
         BT.print(",");
-        BT.println(gz_deg_s);
-        delay(1000);
+        BT.print(gz_deg_s);
+        BT.print(",");
+        BT.println(0); // indicador de alerta
+        
 
-        if (gz_deg_s < -3) { // condicion de alerta
+        if (accel_ang_x < 60) { // condicion de alerta
             a1++;
             delay(1000);
         } else {
@@ -80,18 +101,35 @@ void loop() {
         }
 
         if (a1 > 3) {
+            Serial.print("Alerta----");
+            
+            BT.print(gx_deg_s);
+            BT.print(",");
+            BT.print(gy_deg_s);
+            BT.print(",");
+            BT.print(gz_deg_s);
+            BT.print(",");
             BT.print(1);
+            BT.print(",");
+            BT.println(accel_ang_x);
+            
             digitalWrite(10, 1);
             digitalWrite(11, 1);
             delay(3000);
             digitalWrite(10, 0);
             digitalWrite(11, 0);
         }
-        for(cont = 0; cont < 10; cont ++){ // se toma el promedio de 10 medidas
+        delay(1000);
+        
+    }
+    for(cont = 0; cont < 10; cont ++){ // se toma el promedio de 10 medidas
             myBPM = pulseSensor.getBeatsPerMinute();
             avrBPM += myBPM;
         }
+        avrBPM /= 10;
         // BT.println(avrBPM);
         int testBPM = random(60, 100);
-    }
+        BT.print("p,");
+        
+        BT.println(testBPM);
 }
